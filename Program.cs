fd9337dd42667714feced9be1c512b1bd9fb5f56
rillout <fedkv@gmail.com>
2022-05-10 16:54:18 +0300

    Modified #2 Main & CountResultArray

--- Program.cs
+++ Program.cs
@@ -10,15 +10,16 @@ namespace myFirstQuarterTest
         {
             string[] arrayInput = {"hello", "2", "world", ":-)"};
             int symbolsCount = 3;
+            string[] resultArray = new string[CountResultArray(arrayInput, symbolsCount)];
 
         }
     
-        static int CountResultArray (int[] arrInput, int symbCount)
+        static int CountResultArray (string[] arrInput, int symbCount)
         {
             int count = 0;
             for (int i = 0; i < arrInput.Length; i++)
             {
-                if (arrInput[i] <= symbCount) count++;
+                if (arrInput[i].Length <= symbCount) count++;
             }
             return count;
         }
