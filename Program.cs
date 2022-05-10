0017a06f3bafd9c277b99f33c9ac9ad3f0ba0750
rillout <fedkv@gmail.com>
2022-05-10 16:43:06 +0300

    Modified Main and Flowchart

--- Program.cs
+++ Program.cs
@@ -8,7 +8,8 @@ namespace myFirstQuarterTest
         static void Main(string[] args)
 
         {
-            
+            string[] arrayInput = {"hello", "2", "world", ":-)"};
+            int symbolsCount = 3;
 
         }
     }
